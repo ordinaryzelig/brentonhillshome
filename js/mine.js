94b2dd8b4a1f7e1599eef1b7862b45ad69f7b9ab
Jared Ning <jared@redningja.com>
2018-04-08 15:32:23 -0500

    Add smooth scrolling to all nav links.

--- js/mine.js
+++ js/mine.js
@@ -1,7 +1,7 @@
 jQuery(function($) {
 
   // smooth scroll room nav links.
-  $('#rooms-nav a').on('click', function(e) {
+  $('a.smooth-scroll').on('click', function(e) {
     e.preventDefault();
     $('html,body').animate({
       scrollTop: $($(this).attr('href')).offset().top
